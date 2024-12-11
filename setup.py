54684cb98677e860bc90ecb28fafdb1a66dc40b4
blue <embzhengblue@gmail.com>
2024-12-11 11:27:20 +0800

    1、增加管理模块 2、增加飞书通知模块

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ project_urls = {
 }
 setuptools.setup(
     name="blueutils",
-    version="0.0.2",
+    version="0.0.4",
     author="blue",
     author_email="embzhengblue@gmail.com",
     description="This is a crypto operation tool",
